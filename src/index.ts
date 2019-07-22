a074aa1e571171c4dba0590bb04089c93aea36fd
Eric Mantooth <1023110+erictooth@users.noreply.github.com>
2019-07-22 15:58:50 -0700

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,94 @@
+import rollupCommonJs from "rollup-plugin-commonjs";
+import rollupJson from "rollup-plugin-json";
+import rollupNodeResolve from "rollup-plugin-node-resolve";
+import rollupBabel from "rollup-plugin-babel";
+import rollupSourcemaps from "rollup-plugin-sourcemaps";
+import babelPresetEnv from "@babel/preset-env";
+import babelPluginDefine from "babel-plugin-transform-define";
+import path from "path";
+import fs from "fs";
+import { BuilderOptions, MessageError } from "@pika/types";
+import { rollup } from "rollup";
+
+export async function beforeBuild({ options }: BuilderOptions) {
+    if (!options.name) {
+        throw new MessageError('A "name" option is required for UMD builds.');
+    }
+}
+
+export async function beforeJob({ out }: BuilderOptions) {
+    const srcDirectory = path.join(out, "dist-src/");
+    if (!fs.existsSync(srcDirectory)) {
+        throw new MessageError(
+            '"dist-src/" does not exist, or was not yet created in the pipeline.'
+        );
+    }
+    const srcEntrypoint = path.join(out, "dist-src/index.js");
+    if (!fs.existsSync(srcEntrypoint)) {
+        throw new MessageError(
+            '"dist-src/index.js" is the expected standard entrypoint, but it does not exist.'
+        );
+    }
+}
+
+export function manifest(manifest) {
+    manifest["umd:main"] = "dist-umd/index.bundled.js";
+}
+
+export async function build({ out, options, reporter }: BuilderOptions): Promise<void> {
+    const writeToUmd = path.join(out, "dist-umd", "index.bundled.js");
+
+    const result = await rollup({
+        input: path.join(out, "dist-src/index.js"),
+        plugins: [
+            rollupNodeResolve({
+                preferBuiltins: true,
+                mainFields: ["module", "jsnext", "main", "browser"]
+            }),
+            rollupCommonJs({
+                include: "node_modules/**",
+                sourceMap: !!options.sourcemap,
+                namedExports: options.namedExports
+            }),
+            rollupJson({
+                include: "node_modules/**",
+                compact: true
+            }),
+            rollupBabel({
+                exclude: "",
+                babelrc: false,
+                compact: false,
+                sourceMaps: true,
+                inputSourceMap: false,
+                presets: [
+                    [
+                        babelPresetEnv,
+                        {
+                            spec: true
+                        }
+                    ]
+                ],
+                plugins: [
+                    [
+                        babelPluginDefine,
+                        {
+                            "process.env.NODE_ENV": process.env.NODE_ENV
+                        }
+                    ]
+                ]
+            }),
+            rollupSourcemaps()
+        ],
+        external: options.external
+    });
+
+    await result.write({
+        file: writeToUmd,
+        format: "umd",
+        name: options.name,
+        sourcemap: options.sourcemap || false,
+        globals: options.globals || {}
+    });
+
+    reporter.created(writeToUmd);
+}
